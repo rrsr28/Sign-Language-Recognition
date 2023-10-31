968cb0fcbf130de346018045c60fe6714eb76a52
rrsr28 <rr.sanjayram@gmail.com>
2023-10-31 07:20:46 +0530

    Changes done to the Model Files

--- Models/SVMModel.py
+++ Models/SVMModel.py
@@ -1,51 +1,51 @@
 import pickle
 import pandas as pd
-import seaborn as sns
 from sklearn.svm import SVC
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, f1_score, recall_score, precision_score
+from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score
 
-columns = []
-for i in range(1, 22):
-    columns.extend([f'landmark_{i}_x', f'landmark_{i}_y', f'landmark_{i}_z'])
-columns.append('label')
+class SVMModel:
+    def __init__(self, dataset_file):
+        self.dataset_file = dataset_file
+        self.load_dataset()
+        self.train_model()
 
-data = pd.read_csv('Dataset.csv')
-data.columns = columns
+    def load_dataset(self):
+        columns = []
+        for i in range(1, 22):
+            columns.extend([f'landmark_{i}_x', f'landmark_{i}_y', f'landmark_{i}_z'])
+        columns.append('label')
 
-# Add "No Hands Detected" for rows that are full of zeroes (excluding the label column)
-data.loc[data.iloc[:, :-1].eq(0).all(axis=1), 'label'] = 'No Hand Detected'
+        self.data = pd.read_csv(self.dataset_file)
+        self.data.columns = columns
+        self.data.loc[self.data.iloc[:, :-1].eq(0).all(axis=1), 'label'] = 'No Hand Detected'
 
-X = data.iloc[:, :-1]
-Y = data.iloc[:, -1]
+    def train_model(self):
+        X = self.data.iloc[:, :-1]
+        Y = self.data.iloc[:, -1]
 
-print("Features shape = ", X.shape)
-print("Labels shape = ", Y.shape)
+        X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+        self.svm = SVC(C=10, gamma=0.1, kernel='rbf')
+        self.svm.fit(X_train, y_train)
 
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
-svm = SVC(C=10, gamma=0.1, kernel='rbf')
-svm.fit(X_train, y_train)
+    def evaluate_model(self):
+        y_pred = self.svm.predict(X_test)
+        accuracy = accuracy_score(y_test, y_pred)
+        f1 = f1_score(y_test, y_pred, average='weighted')
+        recall = recall_score(y_test, y_pred, average='weighted')
+        precision = precision_score(y_test, y_pred, average='weighted')
 
-y_pred = svm.predict(X_test)
-print(y_pred)
-print("\n\n")
-print("Accuracy score = ", accuracy_score(y_test, y_pred))
-print("F1 score = ", f1_score(y_test, y_pred, average='weighted'))
-print("Recall score = ", recall_score(y_test, y_pred, average='weighted'))
-print("Precision score = ", precision_score(y_test, y_pred, average='weighted'))
-print("\n\n")
-print("Confusion Matrix")
-print(confusion_matrix(y_test, y_pred))
-print("Classification Report")
-print(classification_report(y_test, y_pred))
+        return accuracy, f1, recall, precision
 
-conf_matrix = confusion_matrix(y_test, y_pred)
-plt.figure(figsize=(8, 6))
-sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', cbar=False)
-plt.xlabel('Predicted Labels')
-plt.ylabel('True Labels')
-plt.title('Confusion Matrix')
-plt.show()
+    def save_model(self, model_filename):
+        pickle.dump(self.svm, open(model_filename, 'wb'))
 
-pickle.dump(svm, open('Models/SVMModel.pkl', 'wb'))
+if __name__ == "__main__":
+    # Replace 'Dataset.csv' with the actual dataset file path
+    svm = SVMModel('Dataset.csv')
+    accuracy, f1, recall, precision = svm.evaluate_model()
+    print("Accuracy:", accuracy)
+    print("F1 Score:", f1)
+    print("Recall Score:", recall)
+    print("Precision Score:", precision)
+    svm.save_model('SVMModel.pkl')
