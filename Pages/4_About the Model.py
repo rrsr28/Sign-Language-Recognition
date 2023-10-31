bc4a902a75c1c8eda036f954d7b06d9697226909
rrsr28 <rr.sanjayram@gmail.com>
2023-10-31 09:09:38 +0530

    Created AboutTheModel Page

---
+++ Pages/4_About the Model.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+# Set Streamlit theme to dark, enable "Run on Save," and set default layout to wide
+st.set_page_config(page_title="Sign Language Recognizer", page_icon="👐", layout="wide")
+
+st.title("About the Model")
+st.subheader("Get some insights about the SVM Model")
