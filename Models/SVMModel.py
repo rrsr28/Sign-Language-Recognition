b776eea06a2cec588dccc349abf31a876e6c49f1
rrsr28 <rr.sanjayram@gmail.com>
2023-10-31 09:02:33 +0530

    Calling Model when new landmarks are collected

--- Models/SVMModel.py
+++ Models/SVMModel.py
@@ -1,16 +1,17 @@
+import os
 import pickle
 import pandas as pd
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score
 
-class SVMModel:
+class SVMModelc:
     def __init__(self, dataset_file):
         self.dataset_file = dataset_file
-        self.load_dataset()
-        self.train_model()
+        self.dataset_file = os.path.abspath(self.dataset_file)
+        self.svmodel()
 
-    def load_dataset(self):
+    def svmodel(self):
         columns = []
         for i in range(1, 22):
             columns.extend([f'landmark_{i}_x', f'landmark_{i}_y', f'landmark_{i}_z'])
@@ -20,7 +21,6 @@ class SVMModel:
         self.data.columns = columns
         self.data.loc[self.data.iloc[:, :-1].eq(0).all(axis=1), 'label'] = 'No Hand Detected'
 
-    def train_model(self):
         X = self.data.iloc[:, :-1]
         Y = self.data.iloc[:, -1]
 
@@ -28,23 +28,17 @@ class SVMModel:
         self.svm = SVC(C=10, gamma=0.1, kernel='rbf')
         self.svm.fit(X_train, y_train)
 
-    def evaluate_model(self):
         y_pred = self.svm.predict(X_test)
         accuracy = accuracy_score(y_test, y_pred)
         f1 = f1_score(y_test, y_pred, average='weighted')
         recall = recall_score(y_test, y_pred, average='weighted')
         precision = precision_score(y_test, y_pred, average='weighted')
 
-        return accuracy, f1, recall, precision
+        print("Accuracy:", accuracy)
+        print("F1 Score:", f1)
+        print("Recall Score:", recall)
+        print("Precision Score:", precision)
 
     def save_model(self, model_filename):
         pickle.dump(self.svm, open(model_filename, 'wb'))
 
-
-svm = SVMModel('Dataset.csv')
-accuracy, f1, recall, precision = svm.evaluate_model()
-print("Accuracy:", accuracy)
-print("F1 Score:", f1)
-print("Recall Score:", recall)
-print("Precision Score:", precision)
-svm.save_model('SVMModel.pkl')
